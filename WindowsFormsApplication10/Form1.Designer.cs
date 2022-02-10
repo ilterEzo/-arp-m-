53e7325b61ea675eb704edce0ddcf37ded3c1a95
ilterEzo <99246243+ilterEzo@users.noreply.github.com>
2022-02-10 11:48:51 +0300

    Add files via upload
    arka plan rengi değişti

--- WindowsFormsApplication10/Form1.Designer.cs
+++ WindowsFormsApplication10/Form1.Designer.cs
@@ -87,6 +87,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1090, 577);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
